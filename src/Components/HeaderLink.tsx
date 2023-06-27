e28630f65d78dbd4ebfa69eb892dd39f8e704014
BonyChops <contact@bonychops.com>
2023-06-28 00:05:36 +0900

    Update: added className

--- src/Components/HeaderLink.tsx
+++ src/Components/HeaderLink.tsx
@@ -6,12 +6,16 @@ export default function HeaderLink(props: {
   children: React.ReactNode;
   href: string;
   onClick?: () => void;
+  className?: string;
 }) {
-  const { children, href, onClick } = props;
+  const { children, href, onClick, className } = props;
   return (
     <CustomLink
       href={href}
-      className="transition text-4xl font-bold  border-b-2 border-transparent hover:border-gray-700"
+      className={
+        "transition text-4xl font-bold  border-b-2 border-transparent hover:border-gray-700 " +
+        (className ?? "")
+      }
       onClick={onClick}
     >
       {children}
