02ca39f229833fd80d6326857ab7445c621153e1
Bony_Chops / Sota Suzuki <contact@bonychops.com>
2023-08-29 12:46:25 +0900

    Update/knowledgework (#7)
    * Update: knowledge work
    
    * Fix: buttom
    
    * Fix: age

--- src/app/TimeLine.tsx
+++ src/app/TimeLine.tsx
@@ -304,10 +304,7 @@ export default function TimeLine() {
             </p>
           </TimeTreeEvent>
         )}
-        <TimeTreeEvent
-          className="opacity-0 transform animate-slideup-delay"
-          noBottom={!showAll}
-        >
+        <TimeTreeEvent className="opacity-0 transform animate-slideup-delay">
           <p className="text-sm opacity-60">2023.07 - 2023.09・20 y.o.</p>
           <p className="text-lg flex font-bold">
             <BuildingOffice2Icon className="h-6 w-6 mr-2 flex-shrink-0 my-auto" />
@@ -315,21 +312,23 @@ export default function TimeLine() {
           </p>
           <p>インターン</p>
         </TimeTreeEvent>
-        {showAll && (
-          <TimeTreeEvent className="opacity-0 transform animate-slideup-delay">
-            <p className="text-sm opacity-60">2023・20 y.o.</p>
-            <p className="text-lg flex font-bold">
-              <BuildingOffice2Icon className="h-6 w-6 mr-2 flex-shrink-0 my-auto" />
-              ？？？
-            </p>
-          </TimeTreeEvent>
-        )}
+        <TimeTreeEvent
+          className="opacity-0 transform animate-slideup-delay"
+          noBottom={!showAll}
+        >
+          <p className="text-sm opacity-60">2023・21 y.o.</p>
+          <p className="text-lg flex font-bold">
+            <BuildingOffice2Icon className="h-6 w-6 mr-2 flex-shrink-0 my-auto" />
+            株式会社ナレッジワーク
+          </p>
+          <p>Enablement Internship for Practical Development（インターン）</p>
+        </TimeTreeEvent>
         {showAll && (
           <TimeTreeEvent
             className="opacity-0 transform animate-slideup-delay"
             noBottom
           >
-            <p className="text-sm opacity-60">2023・20 y.o.</p>
+            <p className="text-sm opacity-60">2023・21 y.o.</p>
             <p className="text-lg flex font-bold">
               <BuildingOffice2Icon className="h-6 w-6 mr-2 flex-shrink-0 my-auto" />
               ？？？
