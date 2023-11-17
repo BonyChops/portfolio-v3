6a9d9a0711b69b83b4f52f0e55197fd53a861c8c
Bony_Chops <contact@bonychops.com>
2023-11-18 00:14:19 +0900

    SkillsにDockerを追加 (#21)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import {
   siAmazonaws,
   siC,
+  siDocker,
   siFirebase,
   siGit,
   siGithub,
@@ -137,6 +138,7 @@ export default async function Home() {
                 TypeScript: siTypescript,
                 "Next.js": siNextdotjs,
                 Go: siGo,
+                Docker: siDocker,
                 AWS: siAmazonaws,
                 Terraform: siTerraform,
               }).map(([k, v], key) => (
