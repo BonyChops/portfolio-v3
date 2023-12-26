997caf70349372e4c5625ca202e2dcd32ebe01da
BonyChops <contact@bonychops.com>
2023-12-26 11:31:53 +0900

    描画に問題があったため

--- src/Components/CommitDate.tsx
+++ src/Components/CommitDate.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { DateTime } from "luxon";
+import { useEffect, useState } from "react";
 
 export default function CommitDate({ commitDate }: { commitDate?: string }) {
-  return (
-    commitDate && (
-      <span title={commitDate}>{`(${DateTime.fromISO(
-        commitDate
-      ).toRelative()})`}</span>
-    )
-  );
+  const [relative, setRelative] = useState<string | null>(commitDate ?? null);
+  useEffect(() => {
+    if (commitDate) {
+      setRelative(DateTime.fromISO(commitDate).toRelative());
+    }
+  }, [commitDate]);
+  return commitDate && <span title={commitDate}>{`(${relative})`}</span>;
 }
