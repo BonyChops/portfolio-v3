27e70e9500d56fec36950fe435da8f0b5087a2b1
BonyChops <contact@bonychops.com>
2024-02-13 12:43:46 +0900

    Refactor CommitDate component to use date-fns library for relative time calculation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,6 @@ import TimeLine from "./TimeLine";
 import { whiteOgpImageIfNotExists, writeOgpImage } from "@/lib/default";
 import { loadGoogleFont } from "@/lib/font";
 import { CustomLink } from "@/Components/CustomLink";
-import { DateTime } from "luxon";
 import CommitDate from "@/Components/CommitDate";
 
 export default async function Home() {
