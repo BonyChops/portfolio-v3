8424be8e4f07b7888b2b472bdcbe2c205644297b
BonyChops <contact@bonychops.com>
2025-04-20 22:06:21 +0900

    fix: gaId

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import './globals.css'
 import 'nprogress/nprogress.css'
-import GoogleAnalytics from '@/Components/GoogleAnalytics'
 import Header from '@/Components/Header'
 import ProgressHandler from '@/Components/ProgressHandler'
+import { GA_MEASUREMENT_ID } from '@/lib/gtag'
 import { generateOGMetadata } from '@/lib/opengraph'
+import { GoogleAnalytics } from '@next/third-parties/google'
 import { Inter } from 'next/font/google'
-import { Suspense } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 const siteName = 'BonyChops'
@@ -26,10 +26,21 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='ja'>
-      <Suspense fallback={<div />}>
-        <GoogleAnalytics />
-      </Suspense>
+    <html suppressHydrationWarning lang='ja'>
+      <GoogleAnalytics gaId={GA_MEASUREMENT_ID} />
+      <head>
+        {/*
+          TypeScript Playground: https://www.typescriptlang.org/play/?target=2#code/BQMwrgdgxgLglgewgAmASmQbwFDOVJAZxmQAcAnAUxEvMOQF5kB3OCAEwWYDoBbAQxhQAFgFlK7OP2AAiYBWq1CAWgIAbBOWWERlXpQBcydv3IBrNDLR9BuwrjzIA-MhknzMh3iMy1cAObCMDIA3A4EEMTIIOQIvADKMJr8-pSMyBpQ-GqJyancqTAAkjB6sgBGAOyE7JQAbqoIGlq8CLVWYXgRUeqaom2Grn6BwcgAPq7uZjKMXqgxcbnkKWkMa66EAJ7EejNjEwsJScupjEwQYGpqGC4KNHTIRodLK2dMvgFBMy4fIzM+U1CDlYHC43E4UDA+ggMHBCEh0JgAFE1HpKDDwYJ+IRKLDeuR+rV0vjCZQwgBfNDoEJAA
+          Terser REPL: https://try.terser.org/
+          Blog: https://blog.stin.ink/articles/how-to-implement-a-perfect-dark-mode
+       */}
+        <script
+          /* biome-ignore lint/security/noDangerouslySetInnerHtml: ダークテーマ切り替え用 */
+          dangerouslySetInnerHTML={{
+            __html: `!function(){const e=window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"light",o=localStorage.getItem("b7sdev-color-mode"),t="system"===o||null==o?e:"light"===o?"light":"dark";window.document.documentElement.dataset.colorMode=t}();`,
+          }}
+        />
+      </head>
       <body className={inter.className}>
         {children}
         <Header />
